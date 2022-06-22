e16c0882e2039bbf541f99f97ae19d925faf4253
yemi <olowofela.olanrewaju@decagon.dev>
2022-06-22 12:46:33 +0100

    new commit

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -2,7 +2,17 @@
  * Implement the solution in this function
  *
  *  * @param {string} value
- */
-function binaryReversal(value) {}
+ **/
+ function binaryReversal(str) {
+  let temp = '';
+  for (let i = 0; i < str.length; i++) {
+      temp = str[i] + temp;
+  }
+  // code goes here
+  return temp;
 
+};
+
+// keep this function call here
+binaryReversal(readline());
 module.exports = binaryReversal;
